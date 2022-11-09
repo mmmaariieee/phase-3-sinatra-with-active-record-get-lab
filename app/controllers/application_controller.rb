41d5e031c54b09263110f1ebb9e5dfb50c3b82cf
Mariia <m.goncharenkoo@icloud.com>
2022-11-09 00:24:47 -0500

    passed tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set default_content_type: "application/json"
 
+  get '/bakeries' do
+    bakeries = Bakery.all 
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all.order(price: :DESC)
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    most_expensive = BakedGood.all.order(price: :DESC).first
+    most_expensive.to_json
+  end
 end
